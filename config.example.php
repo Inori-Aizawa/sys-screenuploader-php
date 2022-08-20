bd6b4919239d8589e869a23236cff32ab5c54b44
inori aizawa <inoriaizawa@proton.me>
2022-08-20 20:44:35 +0200

    Added local file storage
    I removed the telegram bot stuff might make a config option to enable it

--- config.example.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Telegram bot token, start with bot
-// example $bot_id = "bot12345678:AAAAA-BBbbCCddDDeeEE";
-$bot_id = "bot12345678:AAAAA-BBbbCCddDDeeEE";
-//Your chat ID
-// get frome https://api.telegram.org/bot12345678:AAAAA-BBbbCCddDDeeEE/getUpdates
-$chat_id = "1234567890";
-
-$folder = "file/";
--- index.php
+++ index.php
@@ -1,59 +1,44 @@
 <?php
-include "config.php";
 
-$path = $folder.$_REQUEST['filename'];
+include 'config.php'; //A PHP array containing the data that we want to log.
+include 'logging.php'; //A logging function
 
-$ext = strtolower(pathinfo($path, PATHINFO_EXTENSION));
+$path = $folder.getTitleFromName($_REQUEST['filename']).'/'; // get the folder path 
+
+$ext = strtolower(pathinfo($path.$_REQUEST['filename'], PATHINFO_EXTENSION)); // get the extension of the file
 
 // Only image and video
-if(in_array($ext,['jpg','jpeg','png','bmp','gif','mp4','avi','mpg','mpeg'])){
-    file_put_contents($path,file_get_contents('php://input'));
-    if(file_exists($path)){
-        //upload to telegram
-        upload($path);
-        //remove file
-        unlink($path);
+if (in_array($ext, ['jpg', 'jpeg', 'png', 'bmp', 'gif', 'mp4', 'avi', 'mpg', 'mpeg'])) {
+    if (!file_put_contents($path.$_REQUEST['filename'], file_get_contents('php://input'))) { // if the directory does not exsit create it and try again
+        mkdir($path);
+        file_put_contents($path.$_REQUEST['filename'], file_get_contents('php://input'));
     }
-}else{
-    echo "not allowed";
+} else {
+    echo 'not allowed';
+    LogToFile('not allowed');
 }
 
-
-function upload($path)
+function getTitleFromName($filename)
 {
-    global $chat_id, $bot_id;
-    if(file_exists($path)){
-        $bot_url    = "https://api.telegram.org/$bot_id/";
-        $mime = mime_content_type($path);
-        if (strpos($mime, "video") !== false) {
-            $url        = $bot_url . "sendVideo?parse_mode=markdown&chat_id=" . $chat_id;
-            $post_fields = array(
-                'chat_id'   => $chat_id,
-                'supports_streaming' => true,
-                'parse_mode' => 'markdown',
-                'video'     => new CURLFile(realpath($path))
-            );
-        } else {
-            $url        = $bot_url . "sendPhoto?chat_id=" . $chat_id;
-
-            $post_fields = array(
-                'chat_id'   => $chat_id,
-                'parse_mode' => 'markdown',
-                'photo'     => new CURLFile(realpath($path))
-            );
-        }
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            "Content-Type:multipart/form-data"
-        ));
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
-        $output = curl_exec($ch);
-        curl_close($ch);
-        echo $output;
-    }else{
-        return "FILE NOT EXISTS $path\n";
+    $id = (explode('.', explode('-', $filename)[1])[0]);
+    $json = json_decode(file_get_contents('./game_id.json'), true);
+    if (array_key_exists($id, $json)) {
+        return $json[$id];
+    } else {
+        LogToFile('Game does not exsit in database, you can add it yourself with this id: '.$id);
+        return $id;    
     }
-    return $output;
+}
+// from https://www.php.net/manual/en/function.file-put-contents.php#84180
+function file_force_contents($dir, $contents)
+{
+    $parts = explode('/', $dir);
+    $file = array_pop($parts);
+    $dir = '';
+    foreach ($parts as $part) {
+        if (!is_dir($dir .= "/$part")) {
+            mkdir($dir);
+        }
+    }
+    file_put_contents("$dir/$file", $contents);
 }
