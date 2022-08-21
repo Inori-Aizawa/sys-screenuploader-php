75e0e89db9bc8415bc13d96ec95769d4612abc16
inori aizawa <inoriaizawa@proton.me>
2022-08-21 08:11:54 +0200

    Edited config to include telegram parameters

--- index.php
+++ index.php
@@ -9,15 +9,17 @@ $ext = strtolower(pathinfo($path.$_REQUEST['filename'], PATHINFO_EXTENSION)); //
 
 // Only image and video
 if (in_array($ext, ['jpg', 'jpeg', 'png', 'bmp', 'gif', 'mp4', 'avi', 'mpg', 'mpeg'])) {
-    if (!file_put_contents($path.$_REQUEST['filename'], file_get_contents('php://input'))) { // if the directory does not exsit create it and try again
-        mkdir($path);
-        file_put_contents($path.$_REQUEST['filename'], file_get_contents('php://input'));
-    }
+    saveToDisk($path);
 } else {
     echo 'not allowed';
     LogToFile('not allowed');
 }
-
+function saveToDisk($path){
+    if (!file_put_contents($path.$_REQUEST['filename'], file_get_contents('php://input'))) { // if the directory does not exsit create it and try again
+        mkdir($path);
+        file_put_contents($path.$_REQUEST['filename'], file_get_contents('php://input'));
+    }
+}
 function getTitleFromName($filename)
 {
     $id = (explode('.', explode('-', $filename)[1])[0]);
@@ -28,17 +30,4 @@ function getTitleFromName($filename)
         LogToFile('Game does not exsit in database, you can add it yourself with this id: '.$id);
         return $id;    
     }
-}
-// from https://www.php.net/manual/en/function.file-put-contents.php#84180
-function file_force_contents($dir, $contents)
-{
-    $parts = explode('/', $dir);
-    $file = array_pop($parts);
-    $dir = '';
-    foreach ($parts as $part) {
-        if (!is_dir($dir .= "/$part")) {
-            mkdir($dir);
-        }
-    }
-    file_put_contents("$dir/$file", $contents);
 }
